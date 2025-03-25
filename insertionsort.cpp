d4e30b4c91b774a29ec0408b7698d1ecde82f093
maulinakhamidah <maulinakhamidah115@gmail.com>
2025-03-25 11:53:58 +0700

    fungsii void insertionsort

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -47,9 +47,13 @@ for (i = i; i <= n - 1; i++)
 
  while (j >= 0 && arr[j] > temp) // Step 4
 {
-arr[j + 1] = arr[j]; // Step 5
+arr[j + 1] = arr[j]; // Step 4a
+j--; // Step 4b
 }
+
+arr[j + 1] = temp; // Step 5
 }
+
 
 
 }
