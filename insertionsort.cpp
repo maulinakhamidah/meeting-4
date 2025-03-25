5496ddbc5d4a73473794f85e12e82db922295894
maulinakhamidah <maulinakhamidah115@gmail.com>
2025-03-25 11:24:43 +0700

    void input

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -3,3 +3,15 @@ using namespace std;
 
 int arr[20]; // Membuat Array dengan panjang data 20
 int n; // Membuat Variable inputan n
+
+void input()
+{ // Procedure Input
+while (true)
+{
+cout << "masukkan jumlah data pada array : "; // membuat inputan jumlah elememt array
+cin >> n; // memanggil variable inputan n
+
+
+}
+
+}
